308646a5059c1252fefabaf622f09d0f61423aff
dadm <vktm91@nate.com>
2024-06-04 23:49:35 +0900

    feat: test 코드 작성중

---
+++ src/test/java/com/example/demo/order/service/OrderServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.order.service;
+
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    @Test
+    void 회원은_OrderRequestDto_를_이용하여_상품을_구매할_수_있다() {
+
+    }
+
+    @Test
+    void 비회원이_상품을_구매할_시_에러를_던진다() {
+
+    }
+
+    @Test
+    void getMyOrders를_이용하여_구매한_용품_목록을_불러올_수_있다() {
+
+    }
+
+    @Test
+    void getMyOrders를_이용하여_예약중인_용품_목록을_불러올_수_있다() {
+
+    }
+
+    @Test
+    void 판매승인을_통하여_거래를_완료시킬_수_있다() {
+
+    }
+}
---
+++ src/test/java/com/example/demo/product/service/ProductServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.product.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductServiceTest {
+
+    @Test
+    void 회원은_productCreateDto_를_이용하여_상품을_등록할_수_있다() {
+
+    }
+
+    @Test
+    void 비회원이_상품을_등록할_시_에러를_던진다() {
+
+    }
+
+
+}
