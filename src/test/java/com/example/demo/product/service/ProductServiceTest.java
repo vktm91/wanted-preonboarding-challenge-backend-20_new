926963c72144f125c69b58f51edfd9891f9c0c17
dadm <vktm91@nate.com>
2024-06-05 23:45:45 +0900

    feat: test 코드 작성중

--- src/test/java/com/example/demo/product/service/ProductServiceTest.java
+++ src/test/java/com/example/demo/product/service/ProductServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class ProductServiceTest {
 
     @Test
-    void 회원은_productCreateDto_를_이용하여_상품을_등록할_수_있다() {
+    void 회원은_productCreateDto_를_이용하여_상품을_등록_할_수_있다() {
 
     }
 
@@ -14,5 +14,28 @@ public class ProductServiceTest {
 
     }
 
+    @Test
+    void 상품_목록을_불러올_수_있다() {
 
+    }
+
+    @Test
+    void 상품_상세를_불러올_수_있다() {
+
+    }
+
+    @Test
+    void 추가_판매가_가능한_수량이_남아있는_경우_상품_상태가_판매중_이다() {
+
+    }
+
+    @Test
+    void 추가_판매가_불가능하고_현재_구매확정을_대기하고_있는_경우_상품_상태가_예약중_이다() {
+
+    }
+
+    @Test
+    void 모든_수량에_대해_모든_구매자가_모두_구매확정한_경우_상품_상태가_완료_이다() {
+
+    }
 }
