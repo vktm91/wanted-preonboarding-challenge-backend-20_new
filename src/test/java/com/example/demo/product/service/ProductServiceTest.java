94c86f7bf22138df54acefd85bafc8925294c3ea
ddlee <dadamlee@12cm.co.kr>
2024-06-12 21:49:22 +0900

    test: ProductServiceTest 작성중

--- src/test/java/com/example/demo/product/service/ProductServiceTest.java
+++ src/test/java/com/example/demo/product/service/ProductServiceTest.java
@@ -1,17 +1,69 @@
 package com.example.demo.product.service;
 
+import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.mock.TestContainer;
+import com.example.demo.product.domain.Product;
+import com.example.demo.product.domain.ProductCreate;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class ProductServiceTest {
 
     @Test
-    void 회원은_productCreateDto_를_이용하여_상품을_등록_할_수_있다() {
+    void 회원은_상품을_등록_할_수_있다() {
+        // given
+        TestContainer testContainer = TestContainer.builder()
+                .clockHolder(() -> LocalDateTime.of(2024, 6, 9, 0, 0))
+                .build();
 
+        testContainer.userRepository.save(User.builder()
+                .id(1L)
+                .status(UserStatus.ACTIVE)
+                .phone("01012341234")
+                .build());
+
+        ProductCreate productCreate = ProductCreate.builder()
+                .productNm("얌얌쩝쩝 강아지 간식")
+                .productPrice(3000L)
+                .count(3L)
+                .sellerId(1L)
+                .build();
+
+        // when
+        Product result = testContainer.productService.create(productCreate);
+
+        // then
+        assertThat(result.getId()).isEqualTo(1L);
+        assertThat(result.getName()).isEqualTo("얌얌쩝쩝 강아지 간식");
+        assertThat(result.getPrice()).isEqualTo(3000L);
+        assertThat(result.getCount()).isEqualTo(3L);
+        assertThat(result.getSeller().getId()).isEqualTo(1L);
     }
 
     @Test
-    void 비회원이_상품을_등록할_시_에러를_던진다() {
+    void 존재하지_않는_회원ID_를_이용하여_상품을_등록할_시_에러를_던진다() {
+        // given
+        TestContainer testContainer = TestContainer.builder()
+                .clockHolder(() -> LocalDateTime.of(2024, 6, 9, 0, 0))
+                .build();
 
+        ProductCreate productCreate = ProductCreate.builder()
+                .productNm("얌얌쩝쩝 강아지 간식")
+                .productPrice(3000L)
+                .count(3L)
+                .sellerId(1L)
+                .build();
+
+        // when
+        assertThatThrownBy(() -> {
+            testContainer.productController.create(productCreate);
+        }).isInstanceOf(ResourceNotFoundException.class);
     }
 
     @Test
