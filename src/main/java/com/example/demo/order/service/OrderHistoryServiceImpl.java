261f70632219c9fc837384187bd0169bc5a4f3aa
ddlee <dadamlee@12cm.co.kr>
2024-06-20 18:13:17 +0900

    feat: orderHistory update 추가

--- src/main/java/com/example/demo/order/service/OrderHistoryServiceImpl.java
+++ src/main/java/com/example/demo/order/service/OrderHistoryServiceImpl.java
@@ -1,8 +1,14 @@
 package com.example.demo.order.service;
 
+import com.example.demo.common.domain.exception.OrderStatusBadRequestException;
+import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.common.service.port.ClockHolder;
 import com.example.demo.order.controller.port.OrderHistoryService;
 import com.example.demo.order.domain.OrderHistory;
+import com.example.demo.order.domain.OrderHistoryUpdate;
+import com.example.demo.order.domain.OrderStatus;
 import com.example.demo.order.service.port.OrderHistoryRepository;
+import com.example.demo.product.domain.Product;
 import com.example.demo.product.service.port.ProductRepository;
 import com.example.demo.user.service.port.UserRepository;
 import lombok.Builder;
@@ -10,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @Builder
@@ -18,6 +26,7 @@ public class OrderHistoryServiceImpl implements OrderHistoryService {
     private final OrderHistoryRepository orderHistoryRepository;
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
+    private final ClockHolder clockHolder;
 
     @Override
     public List<OrderHistory> getOrderHistoriesByProductNo(long productId) {
@@ -42,5 +51,37 @@ public class OrderHistoryServiceImpl implements OrderHistoryService {
     @Override
     public List<OrderHistory> getOrderHistoriesByPorductIdAndSellerId(long productId, long sellerId) {
         return orderHistoryRepository.findByProduct_IdAndProduct_Seller_Id(productId, sellerId);
+    }
+
+    @Override
+    public OrderHistory updateStatus(OrderHistoryUpdate orderHistoryUpdate) {
+        OrderHistory orderHistory = orderHistoryRepository.findById(orderHistoryUpdate.getOrderHistoryId())
+                .orElseThrow(() -> new ResourceNotFoundException("OrderHistory", orderHistoryUpdate.getOrderHistoryId()));
+
+        validateStatusTransition(orderHistory.getStatus(), orderHistoryUpdate.getStatusTo());
+
+        orderHistory.update(orderHistoryUpdate.getStatusTo(), clockHolder);
+
+        orderHistoryRepository.save(orderHistory);
+
+        Product product = productRepository.findById(orderHistory.getProduct().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Product", orderHistory.getProduct().getId()));
+
+        product.getO
+
+    }
+
+    private void validateStatusTransition(OrderStatus currentStatus, OrderStatus newStatus) {
+        if (newStatus == OrderStatus.APPROVED) {
+            if (currentStatus != OrderStatus.RESERVED) {
+                throw new OrderStatusBadRequestException(currentStatus, newStatus);
+            }
+        } else if (newStatus == OrderStatus.CONFIRMED) {
+            if (currentStatus != OrderStatus.APPROVED) {
+                throw new OrderStatusBadRequestException(currentStatus, newStatus);
+            }
+        } else {
+            throw new OrderStatusBadRequestException();
+        }
     }
 }
