5c082117fd095061e71b658c4c7d5b81adf8ce14
dadm <vktm91@nate.com>
2024-06-24 00:12:01 +0900

    feat: decreaseCount

--- src/main/java/com/example/demo/product/domain/ProductCalculator.java
+++ src/main/java/com/example/demo/product/domain/ProductCalculator.java
@@ -1,12 +1,13 @@
 package com.example.demo.product.domain;
 
 import com.example.demo.common.domain.exception.NegativeProductCountException;
+import com.example.demo.common.domain.exception.ProductStockBadRequestException;
 import com.example.demo.order.domain.OrderHistory;
 import com.example.demo.order.domain.OrderStatus;
 
 import java.util.List;
 
-public class ProductStatusCalculator {
+public class ProductCalculator {
     public static ProductStatus calculateNewStatus(Long newCount, List<OrderHistory> orderHistories) {
         if (newCount < 0) {
             throw new NegativeProductCountException(newCount);
@@ -20,5 +21,13 @@ public class ProductStatusCalculator {
 
             return allConfirmed ? ProductStatus.COMPLETED : ProductStatus.RESERVED;
         }
+    }
+
+    public static Long calculateNewCount(Product product) {
+        if (product.getCount() < 1) {
+            throw new ProductStockBadRequestException();
+        }
+
+        return product.getCount() - 1;
     }
 }
