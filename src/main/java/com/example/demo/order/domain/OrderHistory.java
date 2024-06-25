c4567a4d1ad46316b24eb01850e826f92483b7f5
dadm <vktm91@nate.com>
2024-06-25 18:51:14 +0900

    feat: orderHistory 메서드변경

--- src/main/java/com/example/demo/order/domain/OrderHistory.java
+++ src/main/java/com/example/demo/order/domain/OrderHistory.java
@@ -2,7 +2,6 @@ package com.example.demo.order.domain;
 
 import com.example.demo.common.service.port.ClockHolder;
 import com.example.demo.product.domain.Product;
-import com.example.demo.product.infrastructure.ProductEntity;
 import com.example.demo.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
@@ -31,6 +30,26 @@ public class OrderHistory {
         this.status = status;
         this.registDt = registDt;
         this.updateDt = updateDt;
+    }
+
+    public OrderHistory changeProduct(Product newProduct) {
+        if (this.product != null) {
+            this.product.getOrderHistories().remove(this);
+        }
+
+        OrderHistory newOrderHistory = OrderHistory.builder()
+                .id(this.id)
+                .buyer(this.buyer)
+                .product(newProduct)
+                .price(this.price)
+                .status(this.status)
+                .registDt(this.registDt)
+                .updateDt(this.updateDt)
+                .build();
+
+        newProduct.getOrderHistories().add(newOrderHistory);
+
+        return newOrderHistory;
     }
 
     public static OrderHistory from(User buyer, Product product, ClockHolder clockHolder) {
