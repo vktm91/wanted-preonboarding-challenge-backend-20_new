2e9a9fe92a1b22fc63ecbe90f44b53776398f1cd
ddlee <dadamlee@12cm.co.kr>
2024-06-25 18:51:14 +0900

    fix: orderHistory 메서드변경

--- src/main/java/com/example/demo/order/controller/port/OrderHistoryService.java
+++ src/main/java/com/example/demo/order/controller/port/OrderHistoryService.java
@@ -2,6 +2,7 @@ package com.example.demo.order.controller.port;
 
 import com.example.demo.order.domain.OrderHistory;
 import com.example.demo.order.domain.OrderHistoryUpdate;
+import com.example.demo.user.domain.User;
 
 import java.util.List;
 
@@ -11,5 +12,6 @@ public interface OrderHistoryService {
     List<OrderHistory> getOrderHistoriesBySellerId(long sellerId);
     List<OrderHistory> getOrderHistoriesByProductIdAndBuyerId(long productId, long buyerId);
     List<OrderHistory> getOrderHistoriesByPorductIdAndSellerId(long productId, long sellerId);
+    OrderHistory createOrder(User buyer, Long productId);
     OrderHistory updateStatus(OrderHistoryUpdate orderHistoryUpdate);
 }
