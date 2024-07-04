e831edc7c750f15e58c10368bf261ad0abdc384c
ddlee <dadamlee@12cm.co.kr>
2024-07-04 23:21:04 +0900

    feat: OrderHistoryController 생성

--- src/main/java/com/example/demo/order/controller/OrderHistoryController.java
+++ src/main/java/com/example/demo/order/controller/OrderHistoryController.java
@@ -1,4 +1,42 @@
 package com.example.demo.order.controller;
 
+import com.example.demo.order.controller.port.OrderHistoryService;
+import com.example.demo.order.controller.response.OrderHistoriesResponse;
+import com.example.demo.order.domain.OrderHistoryCreate;
+import com.example.demo.order.domain.OrderHistoryUpdate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrderHistoryController {
+
+    private final OrderHistoryService orderHistoryService;
+
+    @PostMapping
+    public ResponseEntity<OrderHistoriesResponse> create(@RequestBody OrderHistoryCreate orderHistoryCreate) {
+
+        OrderHistoriesResponse orderHistoryResponse = OrderHistoriesResponse.from(orderHistoryService.createOrder(orderHistoryCreate.getBuyerId(), orderHistoryCreate.getProductId()));
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(orderHistoryResponse.getId())
+                .toUri();
+
+        return ResponseEntity.created(location)
+                .body(orderHistoryResponse);
+    }
+
+    @PatchMapping
+    public ResponseEntity<OrderHistoriesResponse> update(@RequestBody OrderHistoryUpdate orderHistoryUpdate) {
+        return ResponseEntity
+                .ok()
+                .body(OrderHistoriesResponse.from(orderHistoryService.updateStatus(orderHistoryUpdate)));
+    }
+
 }
--- src/main/java/com/example/demo/order/controller/response/OrderHistoriesResponse.java
+++ src/main/java/com/example/demo/order/controller/response/OrderHistoriesResponse.java
@@ -1,4 +1,34 @@
 package com.example.demo.order.controller.response;
 
+import com.example.demo.order.domain.OrderHistory;
+import com.example.demo.order.domain.OrderStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@Builder
 public class OrderHistoriesResponse {
+    private final Long id;
+    private final Long buyerId;
+    private final Long productId;
+    private final Long price;
+    private final OrderStatus status;
+    private final LocalDateTime registDt;
+    private final LocalDateTime updateDt;
+
+    public static OrderHistoriesResponse from(OrderHistory orderHistory) {
+        return OrderHistoriesResponse.builder()
+                .id(orderHistory.getId())
+                .buyerId(orderHistory.getBuyer().getId())
+                .productId(orderHistory.getProduct().getId())
+                .price(orderHistory.getPrice())
+                .status(orderHistory.getStatus())
+                .registDt(orderHistory.getRegistDt())
+                .updateDt(orderHistory.getUpdateDt())
+                .build();
+    }
 }
