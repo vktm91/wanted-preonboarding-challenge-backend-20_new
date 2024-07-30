2eced767b56f1e7da9d2bb6c58cf2dfe16cc6f7c
ddlee <dadamlee@12cm.co.kr>
2024-07-31 08:48:27 +0900

    fix: productService에서 orderHistory 파라미터 받도록 변경

--- src/main/java/com/example/demo/product/controller/port/ProductService.java
+++ src/main/java/com/example/demo/product/controller/port/ProductService.java
@@ -1,9 +1,12 @@
 package com.example.demo.product.controller.port;
 
+import com.example.demo.order.domain.OrderHistory;
 import com.example.demo.product.controller.response.ProductResponse;
 import com.example.demo.product.domain.Product;
 import com.example.demo.product.domain.ProductCreate;
 import com.example.demo.product.domain.ProductUpdate;
+
+import java.util.List;
 
 public interface ProductService {
     Product create(ProductCreate productCreate);
@@ -11,6 +14,6 @@ public interface ProductService {
     Product getById(long productId);
     ProductResponse getByIdAndUserId(long productId, Long userId);
     Product updateInfo(long productId, ProductUpdate productUpdate);
-    Product updateStatus(long productId);
-    Product decreaseCount(long productId);
+    Product updateStatus(long productId, List<OrderHistory> orderHistories);
+    Product decreaseCount(long productId, List<OrderHistory> orderHistories);
 }
