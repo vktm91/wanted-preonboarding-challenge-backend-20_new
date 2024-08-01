c1d10b47bff94e6dab711730985081180ae1d40b
dadm <vktm91@nate.com>
2024-08-01 09:33:10 +0900

    test: ProductControllerTest 유효성검사 추가

--- src/test/java/com/example/demo/product/controller/ProductControllerTest.java
+++ src/test/java/com/example/demo/product/controller/ProductControllerTest.java
@@ -1,19 +1,17 @@
 package com.example.demo.product.controller;
 
-import com.example.demo.common.domain.exception.ResourceNotFoundException;
 import com.example.demo.mock.TestContainer;
 import com.example.demo.product.controller.response.ProductResponse;
+import com.example.demo.product.domain.Product;
 import com.example.demo.product.domain.ProductCreate;
+import com.example.demo.product.domain.ProductStatus;
+import com.example.demo.product.domain.ProductUpdate;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
+import jakarta.validation.ValidationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.time.LocalDateTime;
@@ -23,14 +21,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class ProductControllerTest {
 
-    // 유효성 검사
-    // 도메인 모델 / 애플리케이션 레이어(dto) / 인프라스트럭처 레이어(db 필드 제약조건)
-
     @Test
-    void 사용자는_상품을_생성할_수_있다() {
+    void 사용자는_상품을_생성_할_수_있다() {
         // given
+        LocalDateTime fakeLocalDt = LocalDateTime.of(2024, 6, 9, 0, 0);
         TestContainer testContainer = TestContainer.builder()
-                .clockHolder(() -> LocalDateTime.of(2024, 6, 9, 0, 0))
+                .clockHolder(() -> fakeLocalDt)
                 .build();
 
         testContainer.initializeRequestContext("/api/products", 8080, "localhost", "http");
@@ -58,7 +54,10 @@ public class ProductControllerTest {
         assertThat(result.getBody().getId()).isEqualTo(1L);
         assertThat(result.getBody().getName()).isEqualTo("얌얌쩝쩝 강아지 간식");
         assertThat(result.getBody().getPrice()).isEqualTo(3000L);
+        assertThat(result.getBody().getStatus()).isEqualTo(ProductStatus.SALE);
         assertThat(result.getBody().getCount()).isEqualTo(3L);
+        assertThat(result.getBody().getRegistDt()).isEqualTo(fakeLocalDt);
+        assertThat(result.getBody().getUpdateDt()).isNull();
         assertThat(result.getBody().getSeller().getId()).isEqualTo(1L);
         assertThat(result.getHeaders().getLocation()).isEqualTo(uri);
 
@@ -68,15 +67,11 @@ public class ProductControllerTest {
 
 
     @Test
-    void 상품_생성_요청이_부적절할_경우_에러가_난다() {
-        // 상품 생성 요청 시 필수 필드가 누락된 경우 등을 시뮬레이션
+    void 상품_생성_요청값이_누락될_경우_검증오류메시지가_일치한다() {
         // given
         TestContainer testContainer = TestContainer.builder()
                 .clockHolder(() -> LocalDateTime.of(2024, 6, 9, 0, 0))
                 .build();
-
-        testContainer.initializeRequestContext("/api/products", 8080, "localhost", "http");
-        URI uri = testContainer.createUri("/api/products/{id}", 1L);
 
         testContainer.userRepository.save(User.builder()
                 .id(1L)
@@ -85,100 +80,41 @@ public class ProductControllerTest {
                 .build());
 
         ProductCreate productCreate = ProductCreate.builder()
+                .productPrice(0L)
                 .build();
 
-        // when
-        assertThatThrownBy(() -> {
-            testContainer.productController.create(productCreate);
-        }).isInstanceOf(ResourceNotFoundException.class);
-
-
-
-
-        ResponseEntity<ProductResponse> result = testContainer.productController.create(productCreate);
-
-        // then
-        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(result.getBody()).isNotNull();
-        assertThat(result.getBody().getId()).isEqualTo(1L);
-        assertThat(result.getBody().getName()).isEqualTo("얌얌쩝쩝 강아지 간식");
-        assertThat(result.getBody().getPrice()).isEqualTo(3000L);
-        assertThat(result.getBody().getCount()).isEqualTo(3L);
-        assertThat(result.getBody().getSeller().getId()).isEqualTo(1L);
-        assertThat(result.getHeaders().getLocation()).isEqualTo(uri);
-
-        // clean up
-        testContainer.clearRequestContext();
+        // when & then
+        assertThatThrownBy(() -> testContainer.validate(productCreate))
+                .isInstanceOf(ValidationException.class)
+                .hasMessageContaining("Product name must not be blank");
     }
 
-    // getById
 
     @Test
-    void 사용자는_상품을_단건_조회_할_수_있다() {
-
+    void 상품_생성시_상품개수가_1보다_작을_경우_검증오류메시지가_일치한다() {
         // given
+        TestContainer testContainer = TestContainer.builder()
+                .clockHolder(() -> LocalDateTime.of(2024, 6, 9, 0, 0))
+                .build();
 
-        // when
+        testContainer.initializeRequestContext("/api/products", 8080, "localhost", "http");
 
-        // then
+        testContainer.userRepository.save(User.builder()
+                .id(1L)
+                .status(UserStatus.ACTIVE)
+                .phone("01012341234")
+                .build());
 
+        ProductCreate productCreate = ProductCreate.builder()
+                .productNm("얌얌쩝쩝 강아지 간식")
+                .productPrice(3000L)
+                .count(0L)
+                .sellerId(1L)
+                .build();
+
+        // when & then
+        assertThatThrownBy(() -> testContainer.validate(productCreate))
+                .isInstanceOf(ValidationException.class)
+                .hasMessageContaining("Count must be at least 1");
     }
-
-    @Test
-    void 사용자가_존재하지_않는_상품을_조회하는_경우_에러가_난다() {
-
-        // given
-
-        // when
-
-        // then
-
-    }
-
-    @Test
-    void 잘못된_ID_형식으로_상품을_조회하는_경우_에러가_난다() {
-
-        // given
-
-        // when
-
-        // then
-        // 잘못된 ID 형식으로 상품 조회 시나리오 시뮬레이션
-    }
-
-    // update
-
-    @Test
-    void 사용자는_상품을_수정_할_수_있다() {
-
-        // given
-
-        // when
-
-        // then
-
-    }
-
-    @Test
-    void 수정_권한이_없는_상품을_수정하려는_경우_에러가_난다() {
-
-        // given
-
-        // when
-
-        // then
-        // 사용자가 수정 권한이 없는 상품 수정 시나리오 시뮬레이션
-    }
-
-    @Test
-    void 상품_수정_요청이_부적절할_경우_에러가_난다() {
-
-        // given
-
-        // when
-
-        // then
-        // 상품 수정 요청 시 부적절한 바디를 전송한 경우 등을 시뮬레이션
-    }
-
 }
