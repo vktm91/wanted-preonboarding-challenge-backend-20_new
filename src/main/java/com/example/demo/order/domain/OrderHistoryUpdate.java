885187361a140215d98a2323f6b5f16729b07791
ddlee <dadamlee@12cm.co.kr>
2024-08-12 18:53:44 +0900

    fix: order dto validation 설정

--- src/main/java/com/example/demo/order/domain/OrderHistoryUpdate.java
+++ src/main/java/com/example/demo/order/domain/OrderHistoryUpdate.java
@@ -1,17 +1,20 @@
 package com.example.demo.order.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class OrderHistoryUpdate {
-    private final long orderHistoryId;
+    @NotNull(message = "orderHistoryId must not be null")
+    private final Long orderHistoryId;
+    @NotNull(message = "statusTo must not be null")
     private final OrderStatus statusTo;
 
     @Builder
     public OrderHistoryUpdate(
-            @JsonProperty("orderHistoryId") long orderHistoryId,
+            @JsonProperty("orderHistoryId") Long orderHistoryId,
             @JsonProperty("status") OrderStatus statusTo) {
         this.orderHistoryId = orderHistoryId;
         this.statusTo = statusTo;
